766aad3cd9cedbff2b9392b71cbfee242e9510c0
Luthiery Costa <luthierycosta@gmail.com>
2021-10-19 02:46:16 -0300

    Retirando o código antigo comentado

--- queue_manager.py
+++ queue_manager.py
@@ -1,70 +1,38 @@
 from process_manager import Process
 
-""" 
-queue = [[],[]]
-
-i = 0
-
-def schedule(p : Process):
-    global queue
-
-    priority = 0 if p.priority == 0 else 1
-    queue[priority].append(p)
-
-def run():
-    global i
-
-    p = None
-    if len(queue[i]) > 0:
-        p = queue[i].pop(0)
-        p.run()
-    
-        if not p.is_finished():
-            queue[i].append(p)
-    
-    if i == 0:
-        if len(queue[1]) != 0:
-            i = 1
-    else:
-        if len(queue[0]) != 0:
-            i = 0
-
-    return p
-"""
-
 class QueueManager:
     def __init__(self):
         self.queues = [[],[],[],[]]
         self.count = 0
 
-    def schedule(self, process: Process):
+    def schedule(self, proc: Process):
         self.count += 1
         if self.count > 1000:
             print("Limite máximo de processos atingido")
-        i = min(process.priority, 3)
-        self.queues[i].append(process)
+        i = min(proc.priority, 3)
+        self.queues[i].append(proc)
 
     def run(self):
         for (i, queue) in enumerate(self.queues):
             if queue:
-                process = queue.pop(0)
-                process.run()
-                if not process.is_finished():
+                proc = queue.pop(0)
+                proc.run()
+                if not proc.is_finished():
                     if i == 0:
-                        queue.insert(0, process)
+                        queue.insert(0, proc)
                     elif not queue:
-                        queue.append(process)
+                        queue.append(proc)
                     else:
                         next_i = min(i+1, len(self.queues)-1)
-                        self.queues[next_i].append(process)
-                return process
+                        self.queues[next_i].append(proc)
+                return proc
         return None
 
     def print(self):
         string = "Filas:\n"
         for queue in self.queues:
             string += '['
-            for process in queue:
-                string += str(process.pid) + ' '
+            for proc in queue:
+                string += str(proc.pid) + ' '
             string += ']\n'
         return string
